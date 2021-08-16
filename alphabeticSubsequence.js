28913d32ea4d3006b4f115e9a5b9e90cefdfb090
Kate <mushroom.gead@gmail.com>
2021-08-16 22:24:32 +0700

    challenge on 16/08/2021

---
+++ alphabeticSubsequence.js
@@ -0,0 +1,19 @@
+// refer: https://www.udemy.com/course/100-algorithms-challenge/learn/lecture/10510704#overview
+function alphabeticSubsequence(s) {
+    const letters = s.split('');
+    let isSubsequenceLetter = true
+
+    for (let i = 0; i < letters.length - 1 ; i++) {
+        if (letters[i].charCodeAt(0) >= letters[i + 1].charCodeAt(0)) {
+            isSubsequenceLetter = false;
+            break;
+        }
+    }
+    return isSubsequenceLetter
+}
+
+console.log(alphabeticSubsequence('effg')) // false
+console.log(alphabeticSubsequence('cdce')) // false
+
+console.log(alphabeticSubsequence('ace')) // true
+console.log(alphabeticSubsequence('bxz')) // true

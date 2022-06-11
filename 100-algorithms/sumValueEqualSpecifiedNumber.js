98edb9c7d9cb442ec3a1389bd666e556bd7c0a2a
Mushroom Gead <mushroom.gead@gmail.com>
2022-06-11 17:51:32 +0700

    refector: saparate function

--- 100-algorithms/sumValueEqualSpecifiedNumber.js
+++ 100-algorithms/sumValueEqualSpecifiedNumber.js
@@ -1,12 +1,16 @@
 const INITIAL_VALUE = 0;
 
+function sumValue(int1, int2) {
+  return Number(int1) + Number(int2);
+}
+
 function sumValueEqualSpecifiedNumber(specificNumber, min = 0, max = 100) {
   let sumList = [];
   for (let i = 0; i < 100; i++) {
     const sum = String(i)
       .split('')
       .reduce(
-        (prevValue, currentValue) => Number(prevValue) + Number(currentValue),
+        (prevValue, currentValue) => sumValue(prevValue, currentValue),
         INITIAL_VALUE
       );
 
